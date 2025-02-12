6e970411a0a9e91b7cfcbd90165959c5087e2ac3
glaxur <fullstackisaiah@gmail.com>
2025-02-12 11:18:28 -0500

    fixed svg images not showing up from JS

--- script.js
+++ script.js
@@ -1,17 +1,54 @@
-document.getElementById('fetchWeatherBtn').addEventListener('click', fetchWeather);
-function fetchWeather() {
-    fetch('https://wttr.in/?format=j1')
-        .then(response => response.json())
-        .then(data => {
-            const location = data.nearest_area[0].areaName[0].value;
-            const temperature = data.current_condition[0].temp_F;
-            const feelsLike = data.current_condition[0].FeelsLikeF;
-            const description = data.current_condition[0].weatherDesc[0].value;
-            displayWeather(location, temperature, feelsLike, description);
-        })
-        .catch(error => console.error('Error fetching weather data:', error));
+function createAndAppendElement(tag, parent, classNames = [], innerHTML = "") {
+    const element = document.createElement(tag);
+    classNames.forEach((className) => element.classList.add(className));
+    if (innerHTML) element.innerHTML = innerHTML;
+    parent.appendChild(element);
+    return element;
 }
 
-function displayWeather(location, temperature, feelsLike, description) {
-    const weatherInfoDiv = document.getElementById('weatherInfo');
-    weatherInfoDiv.innerHTML = <><p>${location}</p><p>${temperature}°F</p><p>Feels Like: ${feelsLike}</p><p> ${description}</p></>}
+const selectors = [
+    "#container",
+    ".sidebar-container",
+    ".mainbar-container",
+    ".header-container",
+    ".body-container",
+    ".top-container",
+    ".bottom-container",
+    ".left-container",
+    ".right-container",
+    ".first-con",
+    ".second-con",
+    ".left-body-con",
+    ".right-body-con",
+];
+
+const elements = {};
+selectors.forEach(selector => {
+    elements[selector] = document.querySelector(selector);
+});
+
+const elementsToCreate = [
+    { parent: elements[".left-container"], classes: ["search-icon", "search"], content: <span class="mdi mdi-magnify"></span> },
+    { parent: elements[".left-container"], classes: ["search-bar", "search"], content: <input type="text" id="search_bar" name="search_bar"> },
+    { parent: elements[".right-container"], classes: ["notification-icon", "right-div"], content: <span class="mdi mdi-bell-badge-outline"></span> },
+    { parent: elements[".right-container"], classes: ["cat-head-image", "right-div"], content: <img src="./images/catmww.png" alt="cat head image"> },
+    { parent: elements[".right-container"], classes: ["name-text", "right-div"], content: <span class="name-user">MorganOakley</span> },
+    { parent: elements[".first-con"], classes: ["image-large"], content: <img src="./images/catmww.png" alt="cat head image"> },
+    { parent: elements[".first-con"], classes: ["text-large"], content: <span class="greet-user">Hi there,</span><p class="name-morgan">Morgan Oakley (@morgan)</p> },
+];
+
+elementsToCreate.forEach(({ parent, classes, content }) =>
+    createAndAppendElement("div", parent, classes, content)
+);
+
+// Buttons with text to create inside .second-con
+const buttonTexts = ["New", "Upload", "Share"];
+buttonTexts.forEach(text => {
+    createAndAppendElement("button", elements[".second-con"], ["btn"], text);
+});
+
+// Sidebar icons and labels
+iconsData.forEach(iconObj => {
+    const sidebarIcon = createAndAppendElement("div", elements[".sidebar-container"], ["sidebar-icon"], iconObj.icon);
+    createAndAppendElement("span", sidebarIcon, ["label-text"], iconObj.label);
+});
