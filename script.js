e3a6bc239135289ab6c5dd50d9196f0ad7eee888
glaxur <fullstackisaiah@gmail.com>
2025-02-26 11:28:12 -0500

    weather button fix

--- script.js
+++ script.js
@@ -42,3 +42,21 @@ console.log("The sidebar has been loaded");
           html.innerText = item.textBox;
           trendingContainer.appendChild(html);
      });
+
+     document.getElementById('fetchWeatherBtn').addEventListener('onClick', fetchWeather);
+     function fetchWeather() {
+         fetch('https://wttr.in/?format=j1')
+             .then(response => response.json())
+             .then(data => {
+                 const location = data.nearest_area[0].areaName[0].value;
+                 const temperature = data.current_condition[0].temp_F;
+                 const feelsLike = data.current_condition[0].FeelsLikeF;
+                 const description = data.current_condition[0].weatherDesc[0].value;
+                 displayWeather(location, temperature, feelsLike, description);
+             })
+             .catch(error => console.error('Error fetching weather data:', error));
+     }
+
+     function displayWeather(location, temperature, feelsLike, description) {
+         const weatherInfoDiv = document.getElementById('temperatureDisplay');
+         weatherInfoDiv.innerHTML = <><p>${location}</p><p>${temperature}°F</p><p>Feels Like: ${feelsLike}</p><p> ${description}</p></>}
